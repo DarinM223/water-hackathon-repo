28afe6efbd573abcce6846731bd5bec737d950da
Darin Minamoto <darinm223@gmail.com>
2015-08-07 15:29:42 -0700

    Default tab and moved away polling

--- app/scripts/directives/rankings.js
+++ app/scripts/directives/rankings.js
@@ -1,19 +1,31 @@
 'use strict';
 
+
 angular.module('angularApp')
-  .directive('ranking', function($http) {
+  .directive('ranking', function($http, $interval) {
+    var pollRanking = function(scope) {
+      $http
+      .get('http://54.153.46.28/competition')
+      .then(function(response) {
+        scope.tabs = response.data;
+      }, function(response) {
+        console.log('Error: ', response);
+      });
+    };
+
+    var showTab = function(scope, index) {
+      scope.activeIndex = index;
+    };
+
     return {
         restrict: 'E',
         scope: {},
         link: function(scope, element, attrs) {
+          scope.activeIndex = 0;
           scope.tabs = [];
-          $http
-            .get('http://54.153.46.28/competition')
-            .then(function(response) {
-              scope.tabs = response.data;
-            }, function(response) {
-              console.log('Error: ', response);
-            });
+          scope.showTab = showTab.bind(null, scope);
+          pollRanking(scope);
+          // $interval(pollRanking.bind(null, scope), 1000);
         },
         templateUrl: '../../views/ranking-widget.html'
     };
