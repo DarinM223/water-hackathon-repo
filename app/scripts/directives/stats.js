3464277959ad2dce7fcabe86108a594d2343c81a
matthewl <Matthew.l@Taboola.com>
2015-08-07 12:17:43 -0700

    Added empty object

--- app/scripts/directives/stats.js
+++ app/scripts/directives/stats.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*double energyPerHour(double energy, double hour){
+    return energy/hour; 
+    }*/
+
+/*String dailyRanking(int yourRank, int totalPeople){
+    
+  }*/
+
 angular.module('angularApp')
   .directive('stats', function() {
     return {
@@ -7,6 +15,8 @@ angular.module('angularApp')
 	  scope: {}, 
 	  link: function(scope, element, attrs) {
           scope.hello = 'Stats';
+	  //  energyPerHour();
+	  scope.stats = ;
         },
         templateUrl: '../../views/stats-widget.html'
     };
