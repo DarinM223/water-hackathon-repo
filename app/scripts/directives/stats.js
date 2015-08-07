ec6fa0c75d4a376a670b501e25cf8dffc30ab96e
albert.y <albs117@gmail.com>
2015-08-07 16:51:23 -0700

    changes

--- app/scripts/directives/stats.js
+++ app/scripts/directives/stats.js
@@ -11,17 +11,7 @@ angular.module('angularApp')
 	    $http
 		.get('http://54.153.46.28/waterusage')
 		.then(function(response) {
-			for(var i = 0; i < response.data.length; i++)
-			    {
-				if(typeof scope.tabs[i] !== 'undefined' && 
-				   scope.tabs[i] !== null)
-				    {
-					scope.tabs[i] = response.data[i];
-				    }
-
-				else
-				    scope.tabs.push(response.data);
-			    }
+			scope.tabs = response.data;
 			console.log(response.data);
 			//scope.tabs = response.data;
 			
