0cd42d842b4c2840fa7942e42ab5b31e727bd581
matthewl <Matthew.l@Taboola.com>
2015-08-07 12:04:06 -0700

    Added scope stuff

--- app/scripts/directives/stats.js
+++ app/scripts/directives/stats.js
@@ -4,8 +4,10 @@ angular.module('angularApp')
   .directive('stats', function() {
     return {
         restrict: 'E',
-        link: function(scope, element, attrs) {
-          scope.hello = 'Hello!';
+	    scope: {}, //Empty object with a concept of isolate scope 
+	 link: function(scope, element, attrs) {
+          scope.hello = 'Stats';
+	  //scope.stats = 'This is the Stats Section of our code';
         },
         templateUrl: '../../views/stats-widget.html'
     };
