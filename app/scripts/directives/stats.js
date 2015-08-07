a6abbc95675a61d867a353ed2eeef3d0c95f69e2
matthewl <Matthew.l@Taboola.com>
2015-08-07 12:08:18 -0700

    Removed unnecessary stuff

--- app/scripts/directives/stats.js
+++ app/scripts/directives/stats.js
@@ -4,10 +4,9 @@ angular.module('angularApp')
   .directive('stats', function() {
     return {
         restrict: 'E',
-	    scope: {}, //Empty object with a concept of isolate scope 
-	 link: function(scope, element, attrs) {
+	  scope: {}, 
+	  link: function(scope, element, attrs) {
           scope.hello = 'Stats';
-	  //scope.stats = 'This is the Stats Section of our code';
         },
         templateUrl: '../../views/stats-widget.html'
     };
