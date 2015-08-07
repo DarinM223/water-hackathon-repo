b774263efc8e00bb05de062b22ca2730a5eac42b
Darin Minamoto <darinm223@gmail.com>
2015-08-07 16:23:33 -0700

    Fixed stupid flicker, polling works

--- app/scripts/directives/rankings.js
+++ app/scripts/directives/rankings.js
@@ -3,29 +3,45 @@
 
 angular.module('angularApp')
   .directive('ranking', function($http, $interval) {
-    var pollRanking = function(scope) {
-      $http
-      .get('http://54.153.46.28/competition')
-      .then(function(response) {
-        scope.tabs = response.data;
-      }, function(response) {
-        console.log('Error: ', response);
-      });
-    };
 
-    var showTab = function(scope, index) {
-      scope.activeIndex = index;
-    };
 
     return {
         restrict: 'E',
         scope: {},
-        link: function(scope, element, attrs) {
-          scope.activeIndex = 0;
-          scope.tabs = [];
-          scope.showTab = showTab.bind(null, scope);
-          pollRanking(scope);
-          // $interval(pollRanking.bind(null, scope), 1000);
+        bindToController: true,
+        require: ['ranking'],
+        controllerAs: 'rankingController',
+        controller: function() {
+          return;
+        },
+        link: function(scope, element, attrs, ctrls) {
+          var vm = ctrls[0]; 
+
+          var pollRanking = function() {
+            $http
+            .get('http://54.153.46.28/competition')
+            .then(function(response) {
+              for (var i = 0; i < response.data.length; i++) {
+                if (typeof vm.tabs[i] !== 'undefined' && vm.tabs[i] !== null) {
+                  vm.tabs[i] = response.data[i];
+                } else {
+                  vm.tabs.push(response.data);
+                }
+              }
+            }, function(response) {
+              console.log('Error: ', response);
+            });
+          };
+          var showTab = function(index) {
+            vm.activeIndex = index;
+            console.log('New Active Index: ', vm.activeIndex);
+          };
+
+          vm.activeIndex = 0;
+          vm.tabs = [];
+          vm.showTab = showTab;
+          // pollRanking(scope);
+          $interval(pollRanking, 1000);
         },
         templateUrl: '../../views/ranking-widget.html'
     };
