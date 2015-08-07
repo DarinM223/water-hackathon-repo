913c457831fde6faa9c295dfab52fbaa5e648fde
albert.y <albs117@gmail.com>
2015-08-07 14:54:16 -0700

    add some weather stuff

--- app/scripts/directives/recommendations.js
+++ app/scripts/directives/recommendations.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('angularApp')
-  .directive('recommendations', function() {
+  .directive('recommendations', function($http) {
     return {
         restrict: 'E',
         link: function(scope, element, attrs) {
           scope.recommendations = ["a","b","c","d"];
+          scope.weather = {};
+          $http.get('http://api.openweathermap.org/data/2.5/forecast/city?id=6098924&APPID=dbd3904b502508a91efe58edbbe5e589').
+            then(function(response) {
+              scope.weather = response.data;
+              console.log(scope.weather);
+            }, function(response) {
+              console.log('boooo', response);
+            });
         },
         templateUrl: '../../views/recommendations-widget.html'
     };
